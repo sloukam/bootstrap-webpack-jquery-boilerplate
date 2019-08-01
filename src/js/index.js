f5c53767292226820482fde3da547445fc7288cc
martin slouka <mslouka@vendavo.com>
2019-08-01 23:47:16 +0200

    carusel ma nejake demo udalosti

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,15 @@ import 'bootstrap';
 import '../scss/index.scss';
 
 $('#alert').click(() => {
-  alert('jQuery works!');
+  $('#testContent').load('test.html');
+});
+
+$('#alert2').click(() => {
+  $('#testContent2').load('test.html #template2');
+});
+
+$(document).ready(function() {
+  console.log('document is ready...');
 });
 
 // Your jQuery code
