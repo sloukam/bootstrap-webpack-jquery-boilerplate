5f5ac9fcec9bde1a1d3ab8bf16bbe7deb481e92f
martin slouka <mslouka@vendavo.com>
2019-08-18 08:39:31 +0200

    o plemeni - historie + standard

--- src/js/index.js
+++ src/js/index.js
@@ -21,6 +21,10 @@ $(document).ready(function() {
   $('#menu').load('html_parts.html #mainNav');
   $('#news-container-sm').load('html_parts.html #news-content');
   $('#warns-container-sm').load('html_parts.html #warns-content');
+  $('#as-about-container').load('html_parts.html #as-about-content');
+  $('#as-standard-container').load('html_parts.html #as-standard-content');
+
+
   $('#img-carousel-container').load('html_parts.html #img-carousel');
 });
 
